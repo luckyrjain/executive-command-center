737c438eb001ab5258d720d5da845f257e754316
luckyrjain <luckyrjain@gmail.com>
2026-07-13 16:23:16 +0530

    test(backend): satisfy import-order lint

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,8 +1,3 @@
-import os
-
-os.environ.setdefault("ECC_SESSION_SECRET", "test-secret-value-that-is-long-enough")
-os.environ.setdefault("ECC_DATABASE_URL", "sqlite+pysqlite:///:memory:")
-
 from fastapi.testclient import TestClient
 
 from ecc.main import app
