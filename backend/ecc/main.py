cf3eea43724d04c321c2f235d4997d8ea2e9dd26
luckyrjain <luckyrjain@gmail.com>
2026-07-14 19:02:02 +0530

    feat: wire audit query router

--- backend/ecc/main.py
+++ backend/ecc/main.py
@@ -17,6 +17,7 @@ from ecc.domains.governance.risk_mutations import router as risk_mutations_route
 from ecc.domains.governance.risks import router as risks_router
 from ecc.domains.knowledge.notes import router as notes_router
 from ecc.domains.planning.tasks import router as tasks_router
+from ecc.domains.platform.audit_queries import router as audit_queries_router
 from ecc.domains.scheduling.meetings import router as meetings_router
 from ecc.logging import configure_logging
 
@@ -43,6 +44,7 @@ app.include_router(meetings_router)
 app.include_router(risks_router)
 app.include_router(risk_mutations_router)
 app.include_router(attention_router)
+app.include_router(audit_queries_router)
 app.middleware("http")(rejected_mutation_audit_middleware)
 
 
