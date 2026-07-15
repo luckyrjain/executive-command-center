4f48f80084758577c8ce55f9e325de5c19dbc545
luckyrjain <luckyrjain@gmail.com>
2026-07-15 13:51:25 +0530

    feat: wire search and audit workspace

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 import RecommendationPanel from './RecommendationPanel'
+import SearchAuditPanel from './SearchAuditPanel'
 
 const API_BASE = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8000'
 
@@ -223,6 +224,7 @@ export default function App() {
 
       <MorningBrief />
       <RecommendationPanel />
+      <SearchAuditPanel />
     </main>
   )
 }
